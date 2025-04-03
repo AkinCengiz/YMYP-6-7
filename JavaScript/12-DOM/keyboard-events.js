303b55abec19e47abb3889796ae77cfb92281845
akincengiz <akincengiz@outlook.com.tr>
2025-04-03 20:56:21 +0300

    Keyboard events completed

---
+++ JavaScript/12-DOM/keyboard-events.js
@@ -0,0 +1,10 @@
+//keypress() => Numeric, alphanumeric ve character tuşlarında çalışır. (ASCHI code karşılığı olan tuşlarda çalışır.)
+document.addEventListener("keypress",getKeyEvents);
+
+document.addEventListener("keydown",getKeyEvents);
+
+document.addEventListener("keyup",getKeyEvents);
+
+function getKeyEvents(e) {
+    console.log(e.which,e.key,e.type);
+}
